4df0950c8346b3464d4c0f2d8fa6232976c56cba
ReDiculum <rediculum0@runlevel.ch>
2018-10-06 21:21:01 +0200

    :hammer: clock changed from PHP to CSS

--- analog2digital.php
+++ analog2digital.php
@@ -2,6 +2,7 @@
 <head>
 <title>a2d</title>
 <link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="clock.css">
 </head>
 <body>
 <h1 class=center>Analog > Digital</h1>
@@ -10,18 +11,54 @@ function digit($min, $max, $step) {
   return str_pad(round(rand($min, $max)/$step)*$step,2,0,STR_PAD_LEFT);
 }
 
+function hour2deg($value, $min) {
+    $fromRange = 12;
+    $toRange = 360;
+    $offset = $min / 2; // 60min  = 30deg
+    $scaleFactor = $toRange / $fromRange;
+
+    // Re-zero the value within the from range
+    $tmpValue = $value;
+    // Rescale the value to the to range
+    $tmpValue *= $scaleFactor;
+    // Re-zero back to the to range
+    return $tmpValue + $offset;
+}
+
+function min2deg($value) {
+    $fromRange = 60;
+    $toRange = 360;
+    $scaleFactor = $toRange / $fromRange;
+
+    // Re-zero the value within the from range
+    $tmpValue = $value;
+    // Rescale the value to the to range
+    $tmpValue *= $scaleFactor;
+    // Re-zero back to the to range
+    return $tmpValue;
+}
+
 $icon = array("moon.png", "sun.png");
 
 for ($row = 0; $row < 9; $row++) {
   echo "<table><tr>";
-  for ($col = 0; $col < 2; $col++) {?>
-    <td class=right>
+  for ($col = 0; $col < 2; $col++) {
+    $minute = min2deg(digit(15,60,15));
+    $hour = hour2deg(digit(0,11,1), $minute);?>
+    <td class=center>
+    <article class="clock" style="height: 20px;">
+      <div class="minutes-container">
+        <div class="minutes" style="transform: rotate(<?php echo $minute?>deg)"></div>
+      </div>
+      <div class="hours-container">
+        <div class="hours" style="transform: rotate(<?php echo $hour?>deg)"></div>
+      </div>
+    </article>
     <?php
-    echo "<img src=\"clock.php?h=" . digit(0,11,1) . "&m=" . digit(0,45,15) . "\">";
     echo "<img src=" . $icon[rand(0,1)] . " width=20 height=20>";?>
-    </td><td class=right>=&nbsp;____
-    </td><td>&nbsp;:
-    </td><td>____
+    </td><td>=&nbsp;____
+    &nbsp;:
+    ____
     </td>
   <?php }
   echo "</tr></table>";
